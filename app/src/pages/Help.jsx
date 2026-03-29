57c94f30992cecd1192bafd8fb721cfdb3ff326d
nudalomr <nudalomr@mail.uc.edu>
2026-03-29 17:48:05 -0400

    fix Help.jsx

--- app/src/pages/Help.jsx
+++ app/src/pages/Help.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import ContentPanel from "../components/ContentPanel";
-import "./help.css";
+import "../pages/Help.css";
 
 const FAQS = [
   {
